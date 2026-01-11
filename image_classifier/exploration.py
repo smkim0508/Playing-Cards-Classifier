5bf6e6fddccc4f1105676bf1b801f7d76f88acdf
Sungmin Kim <sk3378@princeton.edu>
2026-01-12 01:22:01 +0800

    fix: updated path to handle train, test, validation correctly

--- image_classifier/exploration.py
+++ image_classifier/exploration.py
@@ -24,7 +24,6 @@ print(f"PyTorch version: {torch.__version__}\nTorchvision version: {torchvision.
 
 # Download the latest version of the dataset and cache it locally
 path = kagglehub.dataset_download("gpiosenka/cards-image-datasetclassification")
-
 print(f"Path to downloaded Kaggle dataset files: {path}")
 
 class PlayingCardDataset(Dataset):
@@ -45,25 +44,32 @@ class PlayingCardDataset(Dataset):
     def classes(self):
         return self.data.classes
 
-data_dir = path
+train_path = path + "/train"
+test_path = path + "/test"
+validation_path = path + "/valid"
 
 dataset = PlayingCardDataset(
-    data_dir=data_dir
+    data_dir=train_path
 )
 
 print(f"Length of dataset: {len(dataset)}")
-idx = 535
+idx = 6000
 print(f"Getting random item from idx {idx}: {dataset[idx]}")
 
 image, label = dataset[idx]
 
 # display the image on a pop up window
+# NOTE: this is usually handled natively by Notebooks like Colab
 root = tk.Tk()
-root.title(f"Playing Card {idx}")
+root.title(f"Playing Card {idx}: {label}")
 tk_image = ImageTk.PhotoImage(image)
-label = tk.Label(root, image=tk_image)
-label.pack()
+tk_label = tk.Label(root, image=tk_image)
+tk_label.pack()
 root.mainloop()
 
 # print the label for image from dataset
 print(f"Label: {label}")
+
+# try to map values with folder labels
+target_to_class = {v: k for k, v in dataset.data.class_to_idx.items()}
+print(f"target to class: {target_to_class}")
